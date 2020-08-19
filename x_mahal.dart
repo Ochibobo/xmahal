ac32f91a0f7f8964385f53013dc85a47e166f117
Ochibobo <37383294+Ochibobo@users.noreply.github.com>
2020-08-19 20:59:53 +0300

    Delete x_mahal.dart

--- x_mahal.dart
+++
@@ -1,5 +0,0 @@
-//The file one imports into ones project to use XMahal
-export 'src/xmahal.dart'; // Export XMahal base class (To Mark a class as one to be mapped btwn Dart <--> XMl)
-export 'src/data_formatter.dart'; // Export data formatting class (For primitive-type conversion)
-export 'src/xml_creator.dart'; // Export the XMLCreator class (for converting Dart objects to XML String)
-export 'src/xmodel.dart'; // Export the XModel class (for parsing)
